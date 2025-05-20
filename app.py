d8d96025ee59a60394b1221e1b7768183703ccbb
firman555 <anjaymabar21062003@gmail.com>
2025-05-20 19:53:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def load_data():
     return anime, data
 
 @st.cache_data
-def prepare_matrix(data, num_users=800, num_anime=1000):
+def prepare_matrix(data, num_users=800, num_anime=400):
     top_users = data['user_id'].value_counts().head(num_users).index
     top_anime = data['name'].value_counts().head(num_anime).index
     filtered = data[data['user_id'].isin(top_users) & data['name'].isin(top_anime)]
